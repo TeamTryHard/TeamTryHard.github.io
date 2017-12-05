c069f540ec41b991ede59d77c567786b07df9f29
AmityIunn <jedy2468@gmail.com>
2017-12-05 13:38:49 +0000

    update

--- js.js
+++ js.js
@@ -1,3 +1,54 @@
+$(document).ready(function() {
+
+
+
   $('.pub').on('click', function() {
       $(this).toggleClass('show-description');
   });
+
+
+/*
+
+ $( "#price" ).on('change', function() {
+ 	var selected= $('select').val();
+ 	$('.pub').hide();
+ 	$(selected).show()
+
+  $( "option" ).text( this + "was selected" ).addclass("this").fadeOut( 1000 );
+});
+
+
+
+$('#price').change(function(){
+		var allprice= 
+    	var selected= $('#price').val();
+        if($('selected') == 'all') {
+            $('#pub').removeclass('pub'); 
+        }
+    });
+
+
+ $.viewMap = {
+    '0' : $([]),
+    'view1' : $('#view1'),
+    'view2' : $('#view2a, #view2b'),
+    'view3' : $('#view3')
+  };
+
+  $('#viewSelector').change(function() {
+    // hide all
+    $.each($.viewMap, function() { this.hide(); });
+    // show current
+    $.viewMap[$(this).val()].show();
+  }); */
+$('#price').change(function(){
+	$('.each')('#pub', function() { this.hide(); });
+	$#pub[$(this).val()].show();
+  });
+
+
+
+
+
+
+});
